eaf59a521bfd652c908788bb6daa7aa0b232d482
Xhoxe <111363678+Xhoxe@users.noreply.github.com>
2022-09-13 18:38:42 +0300

    Добавляет Prettier и его конфиг

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,3 @@
 'use strict';
 
-const xNumber = 5;
-const answer = prompt('Укажите любое кол-во целых чисел (через запятаю)');
-
-if (answer === null) {
-    console.log('Ответ не получен!');
-} else {
-    const values = answer.split(',');
-    const result = [];
-    for (let i = 0; i < values.length; i++) {
-        const number = +values[i];
-        if (number !== 0 && number % xNumber === 0) {
-            result.push(number);
-        }
-    }
-    if (result.length > 0) {
-        console.log(`Найдены числа, кратные ${xNumber}, в количестве ${result.length}шт:`);
-        console.log(result.join(', '));
-    } else {
-        console.log(`Числа которые, кратны ${xNumber}, не найдены`)
-    }
-}
+const fn = (x) => x;
